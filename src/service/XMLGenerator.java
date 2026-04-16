f1e0d0116b8dd8d4d2b48696d4e64969ecd160d7
Dox! <144889421+Paradox004@users.noreply.github.com>
2026-04-16 19:25:08 +0200

    Comentarios añadidos

--- src/service/XMLGenerator.java
+++ src/service/XMLGenerator.java
@@ -1,6 +1,12 @@
+/* Alvaro */
 
 package service;
 
+// Interfaz que define el contrato para la generacion del archivo XML del sistema
 public interface XMLGenerator {
+
+    // Genera el archivo XML con todos los datos del sistema (clientes, proveedores, productos y pedidos)
     void generarXML() throws Exception;
 }
+
+/* Alvaro */
