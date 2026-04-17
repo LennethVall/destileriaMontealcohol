aa4181194cded91632b91b3548facad0d0b57a48
on1x <anartz.cabanelas@gmail.com>
2026-04-17 09:11:53 +0200

    Add files via upload
    javadoc nuevo

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"config"},{"l":"dao"},{"l":"model"},{"l":"service"},{"l":"ui"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"config"},{"l":"dao"},{"l":"model"},{"l":"paraLosTeses"},{"l":"service"},{"l":"ui"}];updateSearchResults();
